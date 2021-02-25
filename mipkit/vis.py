5da9fdd48a48d90bb4c5789643fdc34b56d83a33
Cong Vo <congvm.it@gmail.com>
2021-02-25 15:57:49 +0900

    Add vis tools

--- mipkit/vis.py
+++ mipkit/vis.py
@@ -1,9 +1,83 @@
+from .helpers import deprecated
 import os
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import cv2
 import numpy as np
 from .images import read_image
+
+TEXT_COLOR = (255, 255, 255)
+
+
+def get_random_rgb():
+    return (np.random.randint(0, 256), np.random.randint(0, 256), np.random.randint(0, 256))
+
+
+def visualize_bbox(img, bbox, class_name=None, color=None, thickness=2):
+    """Visualizes a single bounding box on the image"""
+    color = color or get_random_rgb()
+    # x_min, y_min, w, h = bbox
+    # x_min, x_max, y_min, y_max = int(x_min), int(
+    #     x_min + w), int(y_min), int(y_min + h)
+    x_min, y_min, x_max, y_max = bbox.astype(int)
+
+    cv2.rectangle(img, (x_min, y_min), (x_max, y_max),
+                  color=color, thickness=thickness)
+
+    if class_name is not None:
+        ((text_width, text_height), _) = cv2.getTextSize(
+            class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)
+
+        cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)),
+                      (x_min + text_width, y_min), color, -1)
+        cv2.putText(
+            img,
+            text=class_name,
+            org=(x_min, y_min - int(0.3 * text_height)),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.5,
+            color=TEXT_COLOR,
+            lineType=cv2.LINE_AA,
+        )
+    return img
+
+
+def convert_coco_to_default(bbox):
+    '''x, y, w, h --> x, y, xx, yy'''
+    bbox_coco = bbox.copy()
+    bbox_coco[:, 2] = bbox_coco[:, 2] + bbox_coco[:, 0]
+    bbox_coco[:, 3] = bbox_coco[:, 3] + bbox_coco[:, 1]
+    return bbox_coco
+
+
+def convert_default_to_coco(bbox):
+    '''x, y, xx, yy --> x, y, w, h'''
+    bbox_coco = bbox.copy()
+    bbox_coco[:, 2] = bbox_coco[:, 2] - bbox_coco[:, 0]
+    bbox_coco[:, 3] = bbox_coco[:, 3] - bbox_coco[:, 1]
+    return bbox_coco
+
+
+def visualize(image, bboxes,
+              scores=None, category_ids=None, category_id_to_name=None,
+              figsize=(12, 12), thickness=2, color=None, bbox_format='default'):
+    img = image.copy()
+
+    scores = [''] * len(bboxes) if scores is None else scores
+    category_ids = [''] * len(bboxes) if category_ids is None else category_ids
+
+    for bbox, sc, category_id in zip(bboxes, scores, category_ids):
+        if category_id_to_name is not None:
+            class_name = category_id_to_name[category_id]
+            class_name = class_name + ' - ' + str(sc)
+        else:
+            class_name = None
+        img = visualize_bbox(img, bbox, class_name,
+                             color=color,
+                             thickness=thickness)
+    plt.figure(figsize=figsize)
+    plt.axis('off')
+    plt.imshow(img)
 
 
 def draw_mask(img_arr, mask_arr):
@@ -15,24 +89,43 @@ def randint(val_min=0, val_max=255):
     return np.random.randint(val_min, val_max)
 
 
-def draw_boxes(img_arr, bboxes, color=None, thickness=2):
+@deprecated(message='draw_boxes() function is deprecated. Please use visualize() instead.')
+def draw_boxes(img_arr, bboxes, color=None, thickness=2, mode='default'):
+    assert mode in ['default', 'coco']
     for box in bboxes:
-        draw_box(img_arr, box, color=color, thickness=thickness)
+        draw_box(img_arr, box, color=color, thickness=thickness, mode=mode)
 
 
-def draw_box(img_arr, box, thickness=2, color=None):
+@deprecated(message='draw_box() function is deprecated. Please use visualize() instead.')
+def draw_box(img_arr, box, thickness=2, color=None, mode='default'):
+    h_img, w_img, c_img = img_arr.shape
     if color is None:
         color = (randint(), randint(), randint())
-    x, y, xx, yy = box
+    print('box', box)
+    if mode == 'default':
+        x, y, xx, yy = box
+        x = int(max(0, x))
+        xx = int(min(w_img, xx))
+        y = int(max(0, y))
+        yy = int(min(h_img, yy))
+
+    elif mode == 'coco':
+        x, y, w, h = box
+        x = int(max(0, x))
+        w = int(min(w_img, x+w_img))
+        y = int(max(0, y))
+        h = int(min(h_img, y+h_img))
+        x, y, xx, yy = x, y, x+w, y+h
+
     cv2.rectangle(img_arr, (x, y), (xx, yy), color=color, thickness=thickness)
 
 
-def show_multi_images(list_img_arr,  
+def show_multi_images(list_img_arr,
                       ratio_size=1,
-                      rows=1, 
-                      plt_show=True, 
-                      title=None, 
-                      wspace=0, 
+                      rows=1,
+                      plt_show=True,
+                      title=None,
+                      wspace=0,
                       hspace=0, *args, **kwargs):
     """Show multiple images in a plot.
 
@@ -50,27 +143,28 @@ def show_multi_images(list_img_arr,
         A title of the figure
     """
     columns = len(list_img_arr)//rows
-    fig = plt.figure(figsize=(int(ratio_size*columns), int((ratio_size/2)*rows)))
-    gs = gridspec.GridSpec(rows, columns, 
-                           wspace=wspace, 
+    fig = plt.figure(
+        figsize=(int(ratio_size*columns), int((ratio_size/2)*rows)))
+    gs = gridspec.GridSpec(rows, columns,
+                           wspace=wspace,
                            hspace=wspace)
-    
+
     for i in range(1, columns*rows + 1):
         a = fig.add_subplot(rows, columns, i)
         plt.imshow(list_img_arr[i - 1])
         a.set_aspect('equal')
         a.set_xticklabels([])
         a.set_yticklabels([])
-        
+
     if title:
         plt.title(title)
-        
+
     if plt_show:
         plt.show()
-        
 
-def show_image_with_paths(list_paths, 
-                          rows=1,  
+
+def show_image_with_paths(list_paths,
+                          rows=1,
                           img_dir=None, **kwargs):
     list_img_arr = []
     for path in list_paths:
