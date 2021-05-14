14f3ea3efd04f49b4caf394b7c30d6e6b70e7b9c
Cong Vo <congvm.it@gmail.com>
2021-05-14 13:45:57 +0900

    Add LICENSE

--- pip_distribute.sh
+++ pip_distribute.sh
@@ -1,3 +1,13 @@
+__='
+   This is the default license template.
+   
+   File: pip_distribute.sh
+   Author: congvm
+   Copyright (c) 2020-2021 congvm
+   
+   To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+'
+
 rm -rf build dist
 python setup.py bdist_wheel
 python -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*  --verbose
